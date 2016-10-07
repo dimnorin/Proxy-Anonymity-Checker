9086922ff553ab40fe67a9a65c69af3cb495bb54
dimnorin <seeking_job@mail.ru>
2016-10-07 12:52:58 +0300

    Second commit

--- src/main/java/dim/scrapper/olx/exception/Responce404Exception.java
+++
@@ -1,9 +0,0 @@
-package dim.scrapper.olx.exception;
-
-import java.io.IOException;
-
-public class Responce404Exception extends IOException {
-	public Responce404Exception(String name){
-		super(name);
-	}
-}
